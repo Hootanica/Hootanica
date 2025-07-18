7b039a90407dff04281c19a72d9faa86df67b477
Chang Huang <cothlory@gmail.com>
2025-07-18 02:14:17 -0400

    fix card delete behavior

--- src/screens/PlantDetailScreen.js
+++ src/screens/PlantDetailScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import PlantContext from '../context/PlantContext';
 
@@ -6,13 +6,26 @@ export default function PlantDetailScreen({ route, navigation }) {
   const { plants, deletePlant } = useContext(PlantContext);
   const plant = plants.find(p => p.id === route.params.id);
 
+  useEffect(() => {
+    if (!plant) {
+      navigation.goBack();
+    }
+  }, [plant]);
+
   return (
     <View style={styles.container}>
-      <Text>Name: {plant.name}</Text>
-      <Text>Type: {plant.type}</Text>
+      <Text>Name: {plant?.name}</Text>
+      <Text>Type: {plant?.type}</Text>
       <Button title="Edit Plant" color={"#074407ff"} onPress={() => navigation.navigate('AddEditPlant', { id: plant.id })} />
       <View style={{ height: 10 }} />
-      <Button title="Delete Plant" color={"#074407ff"} onPress={() => { deletePlant(plant.id); navigation.navigate('Home'); }} />
+      <Button
+        title="Delete Plant"
+        color="#074407ff"
+        onPress={() => {
+          navigation.goBack();
+          deletePlant(plant.id);
+        }}
+      />
     </View>
   );
 }
