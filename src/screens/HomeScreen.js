68bb611a973fb18de36722075525fb6473059767
petalbloom <ga3menon@gmail.com>
2025-07-17 20:29:58 -0400

    Worked on refining home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { View, FlatList, Button, StyleSheet } from 'react-native';
+import { View, FlatList, Button, StyleSheet, Text, SafeAreaView } from 'react-native';
 import PlantContext from '../context/PlantContext';
 import PlantCard from '../components/PlantCard';
 
@@ -7,16 +7,25 @@ export default function HomeScreen({ navigation }) {
   const { plants } = useContext(PlantContext);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View></View>
       <FlatList
         data={plants}
         keyExtractor={item => item.id.toString()}
         renderItem={({ item }) => (
           <PlantCard plant={item} onPress={() => navigation.navigate('PlantDetail', { id: item.id })} />
         )}
+
+        contentContainerStyle={[styles.listContainer, plants.length === 0 && styles.emptyContainer,]}
+        ListEmptyComponent={
+          <Text style={styles.emptyText}>No plants yet. Add your first one!</Text>
+        }
       />
-      <Button title="Add Plant" color={"green"} onPress={() => navigation.navigate('AddEditPlant')} />
-    </View>
+
+      <View style={styles.buttonContainer}>
+      <Button title="Add Plant" color={"#074407ff"} onPress={() => navigation.navigate('AddEditPlant')} />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -24,5 +33,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+    backgroundColor: '#d1f8d1ff', // light green
+  },
+  listContainer: {
+    paddingHorizontal: 16,
+    paddingBottom: 80,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 16,
+    left: 16,
+    right: 16,
+  },
+  emptyContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyText: {
+    fontSize: 32,
+    color: 'gray',
+    textAlign: 'center',
   },
 });
