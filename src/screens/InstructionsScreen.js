db285d04393aaa5775302ed8dfbb70d6eabd48af
petalbloom <ga3menon@gmail.com>
2025-07-23 15:46:10 -0400

    changing instructions screen

--- src/screens/InstructionsScreen.js
+++ src/screens/InstructionsScreen.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { ScrollView, Text, StyleSheet } from 'react-native';
 import Markdown from 'react-native-markdown-display';
 
-const instructions = `
-# Instructions
+const header = `# Welcome to Hootanica!`
 
+const instructions = `
 - Add a plant with its name, type, and photo.
 - Edit or delete existing plants.
 - View plant details and care logs.
@@ -14,6 +14,8 @@ const instructions = `
 export default function InstructionsScreen() {
   return (
     <ScrollView style={styles.container}>
+      <Markdown>{header}</Markdown>
+      
       <Markdown>{instructions}</Markdown>
     </ScrollView>
   );
